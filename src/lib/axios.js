9e3c0c107cd0957df718c61ebe4579ac5a37f243
funayamateppei <t.funayama0217@gmail.com>
2023-02-26 16:26:46 +0900

    デプロイしてからサーバー側にaxiosでリクエストを送ると419エラーで弾かれた。CSRFの問題らしいので、axiosのデフォルトの設定を追記(Axios.js)

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,9 @@ const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-TOKEN': document
+            .querySelector('meta[name="csrf-token"]')
+            .getAttribute('content'),
     },
     withCredentials: true,
 })
