d5b02d16a44c8cba0259e40a857cec46b6ba4c4f
funayamateppei <t.funayama0217@gmail.com>
2023-02-26 16:34:53 +0900

    CSRF問題解消せず、index.jsにmetaタグを追記

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
 
     return (
         <>
+            <meta name="csrf-token" content="{{ csrf_token() }}"></meta>
             <Head>
                 <title>Laravel</title>
             </Head>
