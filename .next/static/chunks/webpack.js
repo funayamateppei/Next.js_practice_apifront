683518ee1cb2a87c6b2d26f45804c46e86dbda87
funayamateppei <t.funayama0217@gmail.com>
2023-02-26 18:21:28 +0900

    axios.js編集

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2ca3615f1ac69502"; }
+/******/ 		__webpack_require__.h = function() { return "1ec7479bbe3293cc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,11 +4,8 @@ const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
-        // 'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
     },
     withCredentials: true,
-    xsrfCookieName: 'CSRF-TOKEN',
-    xsrfHeaderName: 'X-CSRF-Token',
 })
 
 export default axios
