a65b7513ccbc2ffc07cea89d27a7bda9c25c7f07
funayamateppei <t.funayama0217@gmail.com>
2023-02-26 17:40:19 +0900

    axios.jsのデフォルトを編集

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,8 +4,11 @@ const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        // 'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
     },
     withCredentials: true,
+    xsrfCookieName: 'CSRF-TOKEN',
+    xsrfHeaderName: 'X-CSRF-Token',
 })
 
 export default axios
