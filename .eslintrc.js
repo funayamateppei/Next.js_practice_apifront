57da24853e5ac50f86c76cec94672ce463d3cb98
funayamateppei <t.funayama0217@gmail.com>
2023-02-26 16:13:56 +0900

    eslintの設定でNext.jsが使用されていなかったため、.eslintrc.jsのextendsにnextを追記

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
         'eslint:recommended',
         'plugin:react/recommended',
         'plugin:prettier/recommended',
+        'next',
     ],
     parserOptions: {
         ecmaFeatures: {
