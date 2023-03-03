698e1a4c56092421d0f6f3d89423a13e86d3c77d
funayamateppei <t.funayama0217@gmail.com>
2023-03-03 16:40:54 +0900

    build

---
+++ .next/server/chunks/814.js
@@ -0,0 +1,28 @@
+"use strict";
+exports.id = 814;
+exports.ids = [814];
+exports.modules = {
+
+/***/ 5814:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2167);
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
+
+const axios = axios__WEBPACK_IMPORTED_MODULE_0___default().create({
+    baseURL: "http://localhost",
+    headers: {
+        "X-Requested-With": "XMLHttpRequest"
+    },
+    withCredentials: true
+});
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (axios);
+
+
+/***/ })
+
+};
+;
